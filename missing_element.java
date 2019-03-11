9aa39dc82e4f3ab2c655975b5c56528ff718cb54
kdalqarni <36270354+KdAlqarni@users.noreply.github.com>
2019-03-11 17:46:29 -0400

    both lists can be printed directly when there is a change in the list

--- missing_element.java
+++ missing_element.java
@@ -7,19 +7,30 @@ import java.util.Scanner;
 
 public class missing_element {
     public static void main(String[] args) throws Exception {
-        System.out.printf("\nThis program will find the missing element/s that in the First_Array, but NOT\nin the Second_Array\n");
-        int [] array1 = {21, 65, 3, 61, 1, 76};
+        System.out.printf("\nThis program will find the missing element/s in the First_Array but NOT in the Second_Array\n");
+        int [] array1 = {21, 5, 4, 61, 1, 76};
         int [] array2 = {21, 65, 3, 1};
-        System.out.println("The two arrays are:\n\nFirst_Array[21, 65, 3, 61, 1, 76]\nSecond_Array[21, 65, 3, 1]");
-	find_element(array1, array2);
+        System.out.printf("The two arrays are:\n\nFirst_Array: [");
+        for (int elem : array1) {
+            System.out.printf("%d, ",elem);
+        }
+        System.out.printf("]\n");
+        
+        System.out.printf("Second_Array: [");
+        for (int elem : array2) {
+            System.out.printf("%d, ",elem);
+        }
+        System.out.printf("]\n");
+        find_element(array1, array2);
+
     }
+
     public static void find_element(int[] arr1, int[] arr2) {
         int count = 0;
         for (int var : arr1) {
             boolean notMissing = false;
             for (int var2 : arr2){
-                // TODO
-                if (var == var2) {
+                if (var == var2){
                     notMissing = true;
                 }
             }
